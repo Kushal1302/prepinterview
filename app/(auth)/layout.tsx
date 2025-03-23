1bd44e0cda39390e5e29584fd8a2ff2ac2910937
Kushal Kumar <kushalmalviya1302@gmail.com>
2025-03-23 15:08:51 +0530

    feat: next-auth setup done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await getServerSession();
+  console.log(session);
+  if (session) redirect("/");
   return <div className="auth-layout">{children}</div>;
 };
 
